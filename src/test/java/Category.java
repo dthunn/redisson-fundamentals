e5c93c8d419a40cda1bdac0a386bb1ccb0cece9e
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-04-15 15:04:21 -0600

    basics

---
+++ src/test/java/Category.java
@@ -0,0 +1,7 @@
+public enum Category {
+
+    PRIME,
+    STD,
+    GUEST;
+
+}
---
+++ src/test/java/UserOrder.java
@@ -0,0 +1,13 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserOrder {
+    private int id;
+    private Category category;
+}
